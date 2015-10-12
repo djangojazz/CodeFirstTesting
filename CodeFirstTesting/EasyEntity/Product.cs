08b1fcc12c5e15f92012f9ba7e6985821a0cb2ab
djangojazz <brettmorin2@gmail.com>
2015-10-12 12:02:46 -0700

    Made sure some classes got under source control

---
+++ CodeFirstTesting/EasyEntity/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyEntity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Column(TypeName = "varchar")]
+        [Required]
+        [MaxLength(32)]
+        public string ProductName { get; set; }
+    }
+}
---
+++ CodeFirstTesting/EasyEntity/ProductOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyEntity
+{
+    public class ProductOrder
+    {
+        public int ProductOrderId { get; set; }
+        public Person Person { get; set; }
+
+        public List<Product> Products { get; set; }
+        [Column(TypeName = "varchar")]
+        [Required]
+        [MaxLength(32)]
+        public string ProductOrderName { get; set; }
+    }
+}
